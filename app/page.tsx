8821f00da24598d54bab20b9022413a8c828cc57
Steven Carlson <mail@stevencarlson.me>
2025-11-25 21:05:20 -0600

    Add sunset calculator

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
 import { Macondo } from "next/font/google";
+import dayjs from "dayjs";
+import Sunset from "./lib/sunset";
 
 const macondo = Macondo({
   variable: "--font-macondo",
@@ -6,6 +8,12 @@ const macondo = Macondo({
   weight: ["400"],
 });
 
+dayjs.locale("en");
+const now = dayjs().format();
+const weddingDate = dayjs("2026-09-20T15:00:00-06:00");
+const sunsetTime = await Sunset();
+// add a sunset if the sun hasn't set yet today
+const sunsetsRemaining = weddingDate.diff(now, "day") + (sunsetTime > now ? 0 : 1);
 
 export default function Home() {
   return (
@@ -14,8 +22,11 @@ export default function Home() {
         <div className="row">
           <div className="col">
             <h1 className={`${macondo.className} display-1`}>Lydia & Steven</h1>
-            <p>
-              Coming Summer 2026
+            <p className="display-6">
+              Coming Fall 2026
+            </p>
+            <p id="sunsetCounter" className="display-6">
+              <span id="numSunsets">{`${sunsetsRemaining}`}</span> more {`${sunsetsRemaining === 1 ? 'sunset' : 'sunsets'}`}
             </p>
           </div>
         </div>
