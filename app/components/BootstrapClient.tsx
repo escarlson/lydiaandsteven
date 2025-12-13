ac056330c44165d1f8b9434661597500a543ef7d
Steven Carlson <mail@stevencarlson.me>
2025-12-12 22:31:44 -0600

    Fix import

--- app/components/BootstrapClient.tsx
+++ app/components/BootstrapClient.tsx
@@ -4,7 +4,7 @@ import { useEffect } from "react";
 
 function BootstrapClient() {
   useEffect(() => {
-    import("bootstrap/dist/js/bootstrap.bundle.min.js");
+    import("bootstrap");
   }, []);
 
   return null;
