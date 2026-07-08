643ec22ccecdaa796ab96b7cdb9542d05bbf329f
Steven Carlson <mail@stevencarlson.me>
2026-07-07 21:48:09 -0600

    Add missing dot

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Lato } from "next/font/google";
 import "../styles/custom.min.css";
-import "./styles/globals.css";
+import "../styles/globals.css";
 import Header from "./components/Header";
 import BootstrapClient from "./components/BootstrapClient";
 
